b392cae61bb305d7db697892e28f135580872480
Adaobiokeke <adaobiotiji@yahoo.com>
2022-10-03 11:23:12 -0400

    updated resources page

---
+++ src/Resources/resourcedata.js
@@ -0,0 +1,20 @@
+import img1 from '../Assets/picnic-2.jpg'
+import img2 from '../Assets/picnic-img1.jpg'
+import img3 from '../Assets/picnic-3.jpg'
+import img4 from '../Assets/picnic-4.jpg'
+
+
+export const resourcedata = [
+    {
+        image:img1
+    },
+    {
+        image:img2
+    },
+    {
+        image:img3
+    },
+    {
+        image:img4
+    },
+]
