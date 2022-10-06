bb248db3e9eada3af5fc408f59eb7f2642d5a041
Adaobiokeke <adaobiotiji@yahoo.com>
2022-10-06 12:00:30 -0400

    mobile responsiveness

--- src/Activities/Activity.js
+++ src/Activities/Activity.js
@@ -13,11 +13,21 @@ const Activity = () => {
       </section>
       <div className="humanservices">
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={6}>
-            <ReactPlayer
+          <Grid item xs={12} sm={12} md={6} className="vid-cont">
+            <iframe
+              className="vid1"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/u-uFR0FsK7Q"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            {/* <ReactPlayer
               url="https://www.youtube.com/watch?v=u-uFR0FsK7Q"
               className="vid1"
-            />
+            /> */}
           </Grid>
           <Grid item xs={12} sm={12} md={6} className="vid-text">
             <h3>
@@ -27,26 +37,45 @@ const Activity = () => {
         </Grid>
 
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={12} sm={12} md={6} className="vid-cont">
             <h3>
               "Ezeagu Igbudu rendering humanitarian assistance to a villager"
             </h3>
           </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <ReactPlayer
+          <Grid item xs={12} sm={12} md={6} className="vid-cont">
+            <iframe
+              className="vid1"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/o2W9kVvCS6Q"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            {/* <ReactPlayer
               url="https://www.youtube.com/watch?v=o2W9kVvCS6Q"
               className="vid1"
-            />
+            /> */}
           </Grid>
         </Grid>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} md={6}>
-            <ReactPlayer
+          <Grid item xs={12} sm={12} md={6} className="vid-cont">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/ozKx4SdK0Rk"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            {/* <ReactPlayer
               url="https://www.youtube.com/watch?v=ozKx4SdK0Rk"
               className="vid1"
-            />
+            /> */}
           </Grid>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={12} sm={12} md={6} className="vid-cont">
             <h3>
               "Ezeagu Igbudu rendering humanitarian assistance to a villager"
             </h3>
