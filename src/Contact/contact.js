4d560f0547f72de4d14d748290663ab5e6262f66
Adaobiokeke <adaobiotiji@yahoo.com>
2022-12-09 03:06:37 -0500

    update

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -3,62 +3,37 @@ import "./contact.css";
 import Grid from "@material-ui/core/Grid";
 import Phone from "../Assets/contact-img.jpg";
 import Tooltip from "@mui/material/Tooltip";
-import Navc from '../Navbar/Nav-contact'
-import Footer from '../Footer/footer'
+import Navc from "../Navbar/Nav-contact";
+import Footer from "../Footer/footer";
 const Contact = () => {
   return (
     <div>
-      <Navc/>
-    <div className="contact">
-      <Grid container classname="container">
-      <Grid xs={12} sm={6} md={4} className="phone-grid">
-          <img src={Phone} alt="phone-pix" className="phone" />
-        </Grid>
-        {/* <Grid xs={2} sm={2} md={1}></Grid> */}
-        <Grid xs={12} sm={6} md={6} className="contact-det">
-          <h2>
-            We are available for discussions on support for our community and a cup of hot
-            chocolate!
-          </h2>
-          {/* <div className="whatsapp-div">
-          <h3>Call us on </h3>
-          </div> */}
-          {/* <div className="whatsapp">
-          <h2>If talking is not your thing...send us an email on!</h2>
-          <Tooltip title="Click me!" arrow>
-          <a
-            href="https://wa.me/14373455804"
-            className="whatsapp_float"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i class="fa fa-whatsapp whatsapp-icon"></i>
-          </a>
-          </Tooltip>
-          </div> */}
-          <div className="email">
-          <h3 clasName="email">
-            Sending us as email is the fastest way to get our attention for that roubust discussion: 
-            <button className="email">ezeaguigbudutorontocanada@gmail.com</button>
-          </h3>
+      <Navc />
+      <div className="contact">
+        <div className="container">
+          <div className="phone-grid">
+            <img src={Phone} alt="phone-pix" className="phone" />
           </div>
-          {/* <div className="linkedin">
-          <Tooltip title="Click me!" arrow>
-            <a
-              href="https://www.linkedin.com/in/adaobi-okeke-65025258/"
-              target="_blank"
-              className
-              rel="noreferrer"
-            >
-              <button className='linkedin-btn'>Connect with me on LinkedIn</button>
-            </a>
-          </Tooltip>
-          </div> */}
-        </Grid>
-  
-      </Grid>
-    </div>
-    <Footer />
+          {/* <Grid xs={12} sm={12} md={1}></Grid> */}
+          <div className="contact-det">
+            <h2 className="contact-inv">
+              We are available for discussions on support for our community and
+              a cup of hot chocolate!
+            </h2>
+
+            <div className="email">
+              <h3 clasName="email-inv">
+                Sending us an email is the fastest way to get our attention for
+                that roubust discussion:
+                <button className="email-btn">
+                  ezeaguigbudutorontocanada@gmail.com
+                </button>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
